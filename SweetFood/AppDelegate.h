aed8952b31f6b5801121534cbba9770a0966d6c3
范芳芳 <1498012357@qq.com>
2016-03-03 16:10:02 +0800

    Initial Commit

---
+++ SweetFood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SweetFood
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 范芳芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SweetFood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SweetFood
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 范芳芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
